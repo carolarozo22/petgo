721d9508ac4540903816758837d613096dd4a3af
erick.jimenez <erickj.jimenezr@gmail.com>
2021-10-18 11:41:56 -0500

    Se agrega config server

---
+++ ms-configserver/src/test/java/com/peigo/wallet/ms/configserver/msconfigserver/MsConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peigo.wallet.ms.configserver.msconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
