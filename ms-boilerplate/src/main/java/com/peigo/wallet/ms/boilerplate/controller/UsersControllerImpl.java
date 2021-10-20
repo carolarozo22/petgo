96ea6e1423f5fa84cf2b336fd4696fbfaca88201
erick.jimenez <erickj.jimenezr@gmail.com>
2021-10-20 18:06:11 -0500

    Se configura restTemplate con spring-commons

--- ms-boilerplate/src/main/java/com/peigo/wallet/ms/boilerplate/controller/UsersControllerImpl.java
+++ ms-boilerplate/src/main/java/com/peigo/wallet/ms/boilerplate/controller/UsersControllerImpl.java
@@ -3,9 +3,11 @@ package com.peigo.wallet.ms.boilerplate.controller;
 import com.peigo.wallet.ms.boilerplate.model.dto.UserDTO;
 import com.peigo.wallet.ms.boilerplate.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
 
@@ -15,6 +17,9 @@ import javax.validation.Valid;
 public class UsersControllerImpl implements IUsersController{
 
     private final UserService userService;
+
+    @Qualifier("snake_template")
+    private final RestTemplate restTemplate;
 
     @Override
     @PostMapping(value = "/createUser", produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -26,5 +31,10 @@ public class UsersControllerImpl implements IUsersController{
     @GetMapping(value = "/getUsers", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity getFindUser() {
         return ResponseEntity.ok(userService.getUsers());
+    }
+
+    @GetMapping(value = "/getHeaders", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Object getObjectHeaders(){
+        return restTemplate.getForObject("https://httpbin.org/get", Object.class);
     }
 }
