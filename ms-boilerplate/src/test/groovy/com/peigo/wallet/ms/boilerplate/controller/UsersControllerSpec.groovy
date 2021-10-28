e38b4112cb8c41715dc723c2db4e22483901628d
Lisandro <macbook@Lisandros-MacBook-Pro.local>
2021-10-28 08:12:57 -0500

    Adding more asserts for getUsers with jsonPath validations

--- ms-boilerplate/src/test/groovy/com/peigo/wallet/ms/boilerplate/controller/UsersControllerSpec.groovy
+++ ms-boilerplate/src/test/groovy/com/peigo/wallet/ms/boilerplate/controller/UsersControllerSpec.groovy
@@ -1,44 +1,20 @@
 package com.peigo.wallet.ms.boilerplate.controller
 
-import com.amazonaws.services.dynamodbv2.xspec.S
+
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.peigo.wallet.ms.boilerplate.integrationtest.configuration.IntegrationTestConfiguration
 import com.peigo.wallet.ms.boilerplate.model.dto.UserDTO
-import com.peigo.wallet.ms.boilerplate.model.entity.UserEntity
-import com.peigo.wallet.ms.boilerplate.repository.UserRepository
-import com.peigo.wallet.ms.boilerplate.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.MediaType
-import org.springframework.test.util.ReflectionTestUtils
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.web.client.RestTemplate
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
-import software.amazon.awssdk.core.waiters.WaiterResponse
-import software.amazon.awssdk.regions.Region
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient
-import software.amazon.awssdk.services.dynamodb.model.AttributeDefinition
-import software.amazon.awssdk.services.dynamodb.model.CreateTableRequest
-import software.amazon.awssdk.services.dynamodb.model.CreateTableResponse
-import software.amazon.awssdk.services.dynamodb.model.DescribeTableRequest
-import software.amazon.awssdk.services.dynamodb.model.DescribeTableResponse
-import software.amazon.awssdk.services.dynamodb.model.KeySchemaElement
-import software.amazon.awssdk.services.dynamodb.model.KeyType
-import software.amazon.awssdk.services.dynamodb.model.ProvisionedThroughput
-import software.amazon.awssdk.services.dynamodb.model.ScalarAttributeType
-import software.amazon.awssdk.services.dynamodb.waiters.DynamoDbAsyncWaiter
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 
-import java.util.concurrent.CompletableFuture
-
 class UsersControllerSpec extends IntegrationTestConfiguration {
-    String TABLE_NAME = "Users"
-
     private MockMvc mockMvc
 
     @Autowired
@@ -71,11 +47,20 @@ class UsersControllerSpec extends IntegrationTestConfiguration {
     }
 
     def "Get Users"() {
-        expect:"Get Users"
+        expect:
+            getUsers()
+            .andExpect(MockMvcResultMatchers.status().is(200))
+            .andExpect(MockMvcResultMatchers.jsonPath('$.[0].name').isString())
+            .andExpect(MockMvcResultMatchers.jsonPath('$.[0].name').isNotEmpty())
+            .andExpect(MockMvcResultMatchers.jsonPath('$.[0].name').value("ERICK JIMENEZ"))
+            .andExpect(MockMvcResultMatchers.jsonPath('$.[0].email').value("correo.prueba@gmail.com"))
+            .andExpect(MockMvcResultMatchers.jsonPath('$.[0].age').value(20))
+            .andDo(MockMvcResultHandlers.print())
+    }
+    def getUsers() {
         mockMvc.perform(get("/api/boilerplate/getUsers")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .accept(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andDo(MockMvcResultHandlers.print())
+
     }
 }
